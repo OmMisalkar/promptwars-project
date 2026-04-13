a4ed288c9a80ec90e90fd0d716665222452d9fea
Om Misalkar <ommisalkar08@gmail.com>
2026-04-13 19:25:27 +0530

    intial commit

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
 print("Hello PromptWars 🚀")
-resume = input("Enter your resume text: ")
-
-if "python" in resume.lower():
-    print("Good for Software Role ✅")
-else:
-    print("Add Python skills ❌")
